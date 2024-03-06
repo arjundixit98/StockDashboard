27863c446afe1e0bf6e4f388168645eb8f9d795d
Arjun Dixit <arjun.dixit98@gmail.com>
2024-03-07 03:06:05 +0530

    fixed

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ ticker = st.sidebar.text_input('Ticker')
 start_date = st.sidebar.date_input('Start date')
 end_date = st.sidebar.date_input('End Date')
 
+if ticker and start_date and end_date:
+  data = yf.download(ticker,start=start_date, end=end_date)
+  print(data)
+  fig = px.line(data, x=data.index, y = data['Adj Close'], title = ticker)
+  st.plotly_chart(fig)
 
-data = yf.download(ticker,start=start_date, end=end_date)
-fig = px.line(data, x=data.index, y = data['Adj Close'], title = ticker)
-st.plotly_chart(fig)
-
-print('Hello World')
+  print('Hello World')
